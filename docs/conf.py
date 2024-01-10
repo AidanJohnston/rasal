a38448c08401e8baa31ed4d190b15e06e0ef3d80
Aidan Johnston <aidanjhnston@gmail.com>
2024-01-10 11:38:47 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,6 @@ extlinks = {
     "pr": ("https://github.com/AidanJohnston/rasal/pull/%s", "PR #"),
 }
 html_theme = "furo"
-html_theme_path = [furo.get_html_theme_path()]
 html_theme_options = {
     "githuburl": "https://github.com/AidanJohnston/rasal/",
 }
