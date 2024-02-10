9bfa2fbf0dd90bd46a3094b13d4e59508828bf48
Aidan Johnston <aidanjhnston@gmail.com>
2024-02-09 22:35:15 -0500

    lens init

---
+++ src/rasal/lens/lens.cpp
@@ -0,0 +1,15 @@
+#include "rasal.h"
+#include "lens.h"
+
+RASAL_NAMESPACE_OPEN_SCOPE
+
+Lens::Lens()
+{
+
+}
+
+Lens::~Lens()
+{
+}
+
+RASAL_NAMESPACE_CLOSE_SCOPE
---
+++ src/rasal/lens/lens.h
@@ -0,0 +1,19 @@
+#ifndef RASAL_LENS_H
+#define RASAL_LENS_H
+
+#include "rasal.h"
+
+RASAL_NAMESPACE_OPEN_SCOPE
+
+class Lens {
+
+    private:
+
+    public:
+        Lens();
+        ~Lens();
+};
+
+RASAL_NAMESPACE_CLOSE_SCOPE
+
+#endif // RASAl_LENS_H
